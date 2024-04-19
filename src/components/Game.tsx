9fc5b05294872a04a0ce3dfa349ea819e101c4e5
markjso <jo@acenights.com.au>
2024-04-19 14:18:50 +0930

    update

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -4,12 +4,12 @@ const Game: React.FC = () => {
   return (
     <div className="container">
       <div className="game">
-        <h2>Welcome to Pong!</h2>
-	 <h3>Score</h3>
+        <h1>Welcome to Pong!</h1>
+	 <h2>Score</h2>
 	  <p>Player 1: 0  Player 2: 1</p>
         <div className="image-container">
           <a href="https://upload.wikimedia.org/wikipedia/commons/f/f8/Pong.png">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/f/f8/Pong.png" alt="Pong" style={{ width: '500px', height: '211px' }} />
+            <img src="https://upload.wikimedia.org/wikipedia/commons/f/f8/Pong.png" alt="Pong" style={{ width: '800px', height: '338px' }} />
           </a>
         </div>
       </div>
