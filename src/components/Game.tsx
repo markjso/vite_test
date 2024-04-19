0cf910c6ae0f4a90f338ce8fb2e0ea8e185b25bd
Joanne Marks <jmarks@F2N062.42adel.org.au>
2024-04-19 10:37:04 +0930

    update

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -4,14 +4,16 @@ const Game: React.FC = () => {
   return (
     <div className="container">
       <div className="game">
-        <h2>Welcome to  Pong!</h2>
-<a href="https://upload.wikimedia.org/wikipedia/commons/f/f8/Pong.png">
- <img src="https://upload.wikimedia.org/wikipedia/commons/f/f8/Pong.png" alt="Pong" />
-</a>
-  )
+        <h2>Welcome to Pong!</h2>
+        <div className="image-container">
+          <a href="https://upload.wikimedia.org/wikipedia/commons/f/f8/Pong.png">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/f/f8/Pong.png" alt="Pong" />
+          </a>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Game
+export default Game;
+
